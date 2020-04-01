b52d7af435f4e8848453e61cdcb0245d87016e11
orb15 <the.lou.myers@gmail.com>
2020-03-31 20:37:22 -0400

    Extend dice functionality

--- util/dice.go
+++ util/dice.go
@@ -16,12 +16,13 @@ type DiceParseResult struct {
 
 var (
 	xdyPattern = regexp.MustCompile("^([1-9][0-9]*)d([1-9][0-9]*)$")
+	intPattern = regexp.MustCompile("^[0-9]+$")
 )
 
 //ValidateDiceExpr validates and parses a dice expression
 func ValidateDiceExpr(diceExpr, section string, vr *ValidationResult) []*DiceParseResult {
 
-	//safety - should have been checked before this wass called
+	//safety - should have been checked before this was called
 	if diceExpr == "" {
 		vr.Fail(section, "Programming error: call to checkDice with empty dice string!")
 		return nil
@@ -38,27 +39,35 @@ func ValidateDiceExpr(diceExpr, section string, vr *ValidationResult) []*DicePar
 		return nil
 	}
 
-	//consuming parser to validate & parse expressions: xdy +- mdn +- ...
+	//consuming parser to validate & parse expressions: xdy +- mdn +- ... +- N
 	for len(components) > 0 {
+
+		//first, check to see if we have a constant ending eg 2d6 + 3
+		//if we do, we want to consume that specially
+		if intPattern.MatchString(components[0]) {
+			constant, _ := strconv.Atoi(components[0]) //no err here as regex protects
+			pr := &DiceParseResult{
+				Count:    constant,
+				DieType:  0,
+				Operator: "none",
+			}
+			parseResults = append(parseResults, pr)
+			components = components[1:] //consume the constant we just parsed, []string now len = 0
+			continue
+		}
+
+		//we are expecting an xdy pattern at this point or we fail to validate
 		if xdyPattern.MatchString(components[0]) {
 			matches := xdyPattern.FindStringSubmatch(components[0])
-			count, err := strconv.Atoi(matches[1])
-			if err != nil {
-				vr.Fail(section, fmt.Sprintf("Programming error: unable to parse count portion of dice that should be pre-validated: %s!", diceExpr))
-				return nil
-			}
-			dieType, err := strconv.Atoi(matches[2])
-			if err != nil {
-				vr.Fail(section, fmt.Sprintf("Programming error: unable to parse dietype portion of dice that should be pre-validated: %s!", diceExpr))
-				return nil
-			}
+			count, _ := strconv.Atoi(matches[1])   //no err here as regex protects
+			dieType, _ := strconv.Atoi(matches[2]) //no err here as regex protects
 			pr := &DiceParseResult{
 				Count:   count,
 				DieType: dieType,
 			}
 			if len(components) > 1 {
 				switch components[1] {
-				case "+", "-":
+				case "+", "-", "*":
 					pr.Operator = components[1]
 					components = components[2:] //consume the die we just parsed and follow-on operator
 				default:
--- util/dice_test.go
+++ util/dice_test.go
@@ -4,9 +4,10 @@ import (
 	"testing"
 )
 
-func TestIsValidIdentifier_shouldAcceptValidDice(t *testing.T) {
+func TestValidateDiceExpr_shouldAcceptValidDice(t *testing.T) {
 
-	var ids = []string{"1d8", "3d6", "4d2001", "3d6 + 4d8", "1d6 - 7d3"}
+	var ids = []string{"1d8", "3d6", "4d2001", "3d6 + 4d8", "1d6 - 7d3",
+		"1d6 + 2", "3d6 * 100", "1d4 * 2d6 + 3"}
 
 	for _, id := range ids {
 		vr := NewValidationResult()
@@ -17,9 +18,10 @@ func TestIsValidIdentifier_shouldAcceptValidDice(t *testing.T) {
 	}
 }
 
-func TestIsValidIdentifier_shouldRejectInvalidDice(t *testing.T) {
+func TestValidateDiceExpr_shouldRejectInvalidDice(t *testing.T) {
 
-	var ids = []string{"", "0d6", "1d0", "3d6 * 4d8", "d6", "7d", "3d6 +"}
+	var ids = []string{"", "0d6", "1d0", "3d6 / 4d8", "d6", "7d", "3d6 +",
+		"3d6 2d6", "3d6 + 3 + 2d8", "2 + 1d6", "1d6+8", "3d0 + 3"}
 
 	for _, id := range ids {
 		vr := NewValidationResult()
@@ -27,5 +29,68 @@ func TestIsValidIdentifier_shouldRejectInvalidDice(t *testing.T) {
 		if vr.IsValid {
 			t.Errorf("This should be an invalid die expression: %s", id)
 		}
+	}
+}
+
+func TestValidateDiceExpr_shouldParseDiceExpr1(t *testing.T) {
+	vr := NewValidationResult()
+	pde := ValidateDiceExpr("4d6", "testval", vr)
+
+	if len(pde) != 1 {
+		t.Error("Bad parse length")
+	}
+	if pde[0].Count != 4 {
+		t.Error("Bad count")
+	}
+	if pde[0].DieType != 6 {
+		t.Error("Bad die type")
+	}
+	if pde[0].Operator != "none" {
+		t.Error("Bad operator")
+	}
+}
+
+func TestValidateDiceExpr_shouldParseDiceExpr2(t *testing.T) {
+	vr := NewValidationResult()
+	pde := ValidateDiceExpr("4d6 + 3d7 - 1d3 * 21", "testval", vr)
+
+	if len(pde) != 4 {
+		t.Error("Bad parse length")
+	}
+	if pde[0].Count != 4 {
+		t.Error("Bad count")
+	}
+	if pde[0].DieType != 6 {
+		t.Error("Bad die type")
+	}
+	if pde[0].Operator != "+" {
+		t.Error("Bad operator")
+	}
+	if pde[1].Count != 3 {
+		t.Error("Bad count")
+	}
+	if pde[1].DieType != 7 {
+		t.Error("Bad die type")
+	}
+	if pde[1].Operator != "-" {
+		t.Error("Bad operator")
+	}
+	if pde[2].Count != 1 {
+		t.Error("Bad count")
+	}
+	if pde[2].DieType != 3 {
+		t.Error("Bad die type")
+	}
+	if pde[2].Operator != "*" {
+		t.Error("Bad operator")
+	}
+	if pde[3].Count != 21 {
+		t.Error("Bad count")
+	}
+	if pde[3].DieType != 0 {
+		t.Error("Bad die type")
+	}
+	if pde[3].Operator != "none" {
+		t.Error("Bad operator")
 	}
 }

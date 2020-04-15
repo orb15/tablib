102f7614cd00e32ec5948efaa0b4b09aceffd3c6
orb15 <the.lou.myers@gmail.com>
2020-04-15 18:18:01 -0400

    Search tests

--- searchexe_test.go
+++ searchexe_test.go
@@ -37,7 +37,226 @@ func TestExecuteSearch_shouldReturnFullRepoOnNoParams(t *testing.T) {
 	cr.AddTable([]byte(yml2))
 	cr.AddLuaScript("foo", lua, []string{"tag3"})
 	sr, _ := cr.Search("", nil)
+	if len(sr) != 3 {
+		t.Errorf("Unexpected search result length")
+	}
 	res := compareSearchResults(sr, sryaml1, sryaml2, srlua1)
+	for n, r := range res {
+		if !r {
+			t.Errorf("Item: %s failed", n)
+		}
+	}
+}
+
+func TestExecuteSearch_shouldReturnTagSearchWithoutDuplicates(t *testing.T) {
+	yml1 := `
+  definition:
+    name: test
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag1
+      - tag2
+  content:
+    - item 1`
+
+	yml2 := `
+  definition:
+    name: test2
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag2
+  content:
+    - item 1`
+
+	lua := `
+  print("dlrow olleh")
+  `
+	sryaml1 := &SearchResult{Name: "test", Type: itemTypeTable, Tags: []string{"tag1", "tag2"}}
+	sryaml2 := &SearchResult{Name: "test2", Type: itemTypeTable, Tags: []string{"tag2"}}
+
+	cr := newConcreteRepo()
+	cr.AddTable([]byte(yml1))
+	cr.AddTable([]byte(yml2))
+	cr.AddLuaScript("foo", lua, []string{"tag3"})
+	sr, _ := cr.Search("", []string{"tag1", "tag2"})
+	if len(sr) != 2 {
+		t.Errorf("Unexpected search result length")
+	}
+	res := compareSearchResults(sr, sryaml1, sryaml2)
+	for n, r := range res {
+		if !r {
+			t.Errorf("Item: %s failed", n)
+		}
+	}
+}
+
+func TestExecuteSearch_shouldReturnTagSearchWithoutNameConflicts(t *testing.T) {
+	yml1 := `
+  definition:
+    name: test
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag1
+      - tag2
+  content:
+    - item 1`
+
+	yml2 := `
+  definition:
+    name: test2
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag2
+  content:
+    - item 1`
+
+	lua := `
+  print("dlrow olleh")
+  `
+	sryaml1 := &SearchResult{Name: "test", Type: itemTypeTable, Tags: []string{"tag1", "tag2"}}
+	srlua1 := &SearchResult{Name: "test", Type: itemTypeScript, Tags: []string{"tag1"}}
+
+	cr := newConcreteRepo()
+	cr.AddTable([]byte(yml1))
+	cr.AddTable([]byte(yml2))
+	cr.AddLuaScript("test", lua, []string{"tag1"})
+	sr, _ := cr.Search("", []string{"tag1"})
+	if len(sr) != 2 {
+		t.Errorf("Unexpected search result length")
+	}
+	res := compareSearchResults(sr, sryaml1, srlua1)
+	for n, r := range res {
+		if !r {
+			t.Errorf("Item: %s failed", n)
+		}
+	}
+}
+
+func TestExecuteSearch_shouldSearchByNameOnly(t *testing.T) {
+	yml1 := `
+  definition:
+    name: test
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag1
+  content:
+    - item 1`
+
+	yml2 := `
+  definition:
+    name: test2
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag2
+  content:
+    - item 1`
+
+	lua := `
+  print("dlrow olleh")
+  `
+	sryaml1 := &SearchResult{Name: "test", Type: itemTypeTable, Tags: []string{"tag1"}}
+	sryaml2 := &SearchResult{Name: "test2", Type: itemTypeTable, Tags: []string{"tag2"}}
+
+	cr := newConcreteRepo()
+	cr.AddTable([]byte(yml1))
+	cr.AddTable([]byte(yml2))
+	cr.AddLuaScript("foo", lua, []string{"tag3"})
+	sr, _ := cr.Search("test.*", nil)
+	if len(sr) != 2 {
+		t.Errorf("Unexpected search result length")
+	}
+	res := compareSearchResults(sr, sryaml1, sryaml2)
+	for n, r := range res {
+		if !r {
+			t.Errorf("Item: %s failed", n)
+		}
+	}
+}
+
+func TestExecuteSearch_shouldSearchByNameFailsOnBadRegex(t *testing.T) {
+	yml1 := `
+  definition:
+    name: test
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag1
+  content:
+    - item 1`
+
+	yml2 := `
+  definition:
+    name: test2
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag2
+  content:
+    - item 1`
+
+	lua := `
+  print("dlrow olleh")
+  `
+
+	cr := newConcreteRepo()
+	cr.AddTable([]byte(yml1))
+	cr.AddTable([]byte(yml2))
+	cr.AddLuaScript("foo", lua, []string{"tag3"})
+	_, err := cr.Search("(", nil)
+	if err == nil {
+		t.Error("Did not fail on bad regex as expected")
+	}
+}
+
+func TestExecuteSearch_shouldSearchByTagsThenName(t *testing.T) {
+	yml1 := `
+  definition:
+    name: test
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag1
+  content:
+    - item 1`
+
+	yml2 := `
+  definition:
+    name: test2
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag2
+  content:
+    - item 1`
+
+	yml3 := `
+  definition:
+    name: test3
+    type: flat
+    note: this is an optional note
+    tags:
+      - tag1
+  content:
+    - item 1`
+
+	sryaml3 := &SearchResult{Name: "test3", Type: itemTypeTable, Tags: []string{"tag1"}}
+
+	cr := newConcreteRepo()
+	cr.AddTable([]byte(yml1))
+	cr.AddTable([]byte(yml2))
+	cr.AddTable([]byte(yml3))
+
+	sr, _ := cr.Search("test[0-9]+", []string{"tag1"})
+	if len(sr) != 1 {
+		t.Errorf("Unexpected search result length")
+	}
+	res := compareSearchResults(sr, sryaml3)
 	for n, r := range res {
 		if !r {
 			t.Errorf("Item: %s failed", n)
